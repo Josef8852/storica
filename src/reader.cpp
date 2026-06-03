c5d76f65bd6915dbcec767a7f7739a1340fa6637
Josef8852 <josefaboshady2@gmail.com>
2026-06-03 03:04:10 +0200

    remove comments

--- src/reader.cpp
+++ src/reader.cpp
@@ -11,28 +11,29 @@ vector<Commit> readCommits(const string &path) {
 
     git_repository *repo = nullptr ;
     
-    int repo_error = git_repository_open(&repo, path.c_str()); // == 0 success , < 0 failed
+    int repo_error = git_repository_open(&repo, path.c_str()); 
 
     if(repo_error  < 0) {
         throw runtime_error("Failed to open repo at: " + path);
     }
 
-    git_revwalk *walker = nullptr ; // pointer to commit objects
+    git_revwalk *walker = nullptr ; 
 
     int walker_error = git_revwalk_new(&walker, repo);
 
-    git_revwalk_sorting(walker, GIT_SORT_TIME); // sort by timestamp
+    git_revwalk_sorting(walker, GIT_SORT_TIME); 
     
-    walker_error = git_revwalk_push_head(walker); // point to repo head
+    walker_error = git_revwalk_push_head(walker); 
 
     if(walker_error < 0) {
-         git_revwalk_free(walker); // free memory
+         git_revwalk_free(walker); 
          throw runtime_error("Failed to access repository HEAD");
     }
     
     git_oid oid;
     while (!git_revwalk_next(&oid, walker)) {
-        git_commit *commit;
+        
+        git_commit *commit = nullptr;
         int commit_error = git_commit_lookup(&commit, repo, &oid);
 
         if(commit_error < 0) {
