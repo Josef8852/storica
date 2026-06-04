eca8a93b8c4d1052ecd051f074c90b4336e6c912
Josef8852 <josefaboshady2@gmail.com>
2026-06-04 19:23:12 +0200

    implement loadCache

--- src/cache.cpp
+++ src/cache.cpp
@@ -1,4 +1,5 @@
 #include "../include/cache.hpp"
+#include <cstdint>
 #include <fstream>
 #include <nlohmann/json.hpp>
 
@@ -38,3 +39,43 @@ void saveCache(const vector<Commit> &commits, const string &path) {
     file << data ;
     
 };
+
+
+vector<Commit> loadCache(const std::string &cachePath) {
+
+    vector<Commit> commits ; 
+
+    ifstream file(cachePath) ; 
+    if(!file.is_open()) {
+        return {} ;
+    }
+
+    try {
+         json data = json::parse(file);
+         for(const auto &commitJson : data["commits"]) {
+             Commit commit ; 
+             commit.author.name = commitJson["name"].get<string>();
+             commit.author.email = commitJson["email"].get<string>();
+             commit.hash = commitJson["hash"].get<string>();
+             commit.message = commitJson["message"].get<string>();
+             commit.timeStamp = commitJson["timeStamp"].get<int64_t>();
+
+
+             for(const auto &fc : commitJson["fileChanges"]) {
+                 commit.fileChanges.push_back({
+                    fc["fileChanged"], 
+                    fc["linesAdded"],
+                    fc["linesDeleted"]
+                 });
+             }
+             
+             commits.push_back(commit);
+         }
+    }
+    catch(...) {}
+    
+
+
+
+    return commits ; 
+}
